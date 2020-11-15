28d6bb7055c2dafda9cb43e013c4dbb73983ce18
AlexSyrov <1854105@gmail.com>
2020-11-15 21:12:35 +0200

    разметка рендерится, данные приходят с бэкэнда но не попадают в HTML файл(2)

--- src/index.js
+++ src/index.js
@@ -13,17 +13,17 @@ function onSearch(event) {
     event.preventDefault();
 
     const form = event.currentTarget;
-    const saerchQuery = form.elements.query.value;
+    const query = form.elements.query.value;
 
-    fethcCountries(saerchQuery)
+    fethcCountries(query)
         .then(renderCountryCard)
         .catch(error => console.log(error));
 
  };
 
-function fethcCountries(countryName) {
-    
-    return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+function fethcCountries(saerchQuery) {          
+
+    return fetch(`https://restcountries.eu/rest/v2/name/${saerchQuery}`)
         .then(response => {
         return response.json();
     });
@@ -31,9 +31,16 @@ function fethcCountries(countryName) {
 
 function renderCountryCard(country) {
     const markup = countryTpl(country);
-    refs.cardContainer.innerHTML = markup;
+    refs.cardContainer.innerHTML = markup; 
 }   
 
 
 
 
+// fetch(`https://restcountries.eu/rest/v2/name/${saerchQuery}`)
+//     .then(response => {
+//         return response.json();
+//     }).then(country => {
+//         const markup = countryTpl(country);
+//         refs.cardContainer.innerHTML = markup;
+//     }).catch(error => console.log(error));
